f8c8872698fa20bda30f320ee30dc3982242133b
drfn <driftedn@gmail.com>
2022-10-09 13:26:37 +0800

    bad techtree

--- src/ws/content/WTree.java
+++ src/ws/content/WTree.java
@@ -15,43 +15,7 @@ import static mindustry.content.TechTree.*;
 public class WTree{
 	public static void load(){	
 		nodeRoot("windst", start, () -> {
-			// items
-			nodeProduce(axonium, () -> {
-				nodeProduce(terron, Seq.with(new Produce(axonium)), () -> {});
-				nodeProduce(notanium, Seq.with(new Produce(axonium)), () -> {
-					nodeProduce(ectonite, Seq.with(new Produce(notanium)), () -> {});
-					nodeProduce(chasmium, Seq.with(new Produce(notanium)), () -> {});
-				});
-			});
-
-			// blocks
-			// defense
-			node(axoniumWall, () -> {
-				node(axoniumWallLarge, () -> {
-					node(axoniumWallHuge, () -> {
-						node(notaniumWall, () -> {
-							node(notaniumWallLarge, () -> {
-								node(notaniumWallHuge);
-							});
-						});
-						// chasmium/ectonite walls
-					});
-				});
-				node(disposableWall, () -> {
-					node(disposableWallLarge, () -> {
-						node(disposableWallHuge);
-					});
-				});
-			});
-
-			// distribution
-			node(axoBridge, () -> {
-				node(axoRouter);
-				node(terronBridge, () -> {
-					node(notaniumGate);
-					node(notaniumSorter);
-				});
-			});
-        });
+			// todo
+		});
     }
 }
